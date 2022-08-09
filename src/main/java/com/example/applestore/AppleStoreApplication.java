df62040bfa0d57de6fdd776ee6f07099ab6906a4
ytaoCrow <kafka880407@gmail.com>
2022-08-09 08:43:34 +0800

    try push - 4

---
+++ src/main/java/com/example/applestore/AppleStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.applestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/applestore/AppleStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
